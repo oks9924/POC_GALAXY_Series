129946a5b8164a54e3911731b4713bf9e7d7fe27
오경식 <nuke1030@gmail.com>
2025-12-17 16:22:52 +0900

    POCG-1604 Create network driver skeleton and header

---
+++ net_driver.c
@@ -0,0 +1,11 @@
+#include "net_driver.h"
+
+// 내부 함수 선언 (Stub)
+static bool init_hardware_interface(void) { return true; } // 임시 return
+static void enable_dma(void) {}
+static void configure_interrupts(void) {}
+
+bool init_network_interface(void) {
+    // 골격만 존재, 아직 로직 없음
+    return false; 
+}
